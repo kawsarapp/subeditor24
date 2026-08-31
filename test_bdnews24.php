5776439dd5b443a0018bda7c1253998a2c8f85e1
Kawsar <kawsarapps72@gmail.com>
2026-08-31 18:31:55 +0600

    🔧 Database seeders update & cleanup

--- test_bdnews24.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Show first 3000 chars of bdnews24 HTML to understand structure
-$html = file_get_contents('out_bdnews24.html');
-echo "Total length: " . strlen($html) . "\n";
-echo substr($html, 0, 3000);
